b414e133ca87f61c8e234abeab7b86c288fdff91
Dakota Vikdal <dakota.james19@hotmail.com>
2023-02-27 20:34:58 -0500

    Big win

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,23 @@
+import {useState} from "react"
 import React from "react";
 
-function Item({ name, category }) {
+function Item({ name, category }) {  
+
+  const [itemList, setItemList] = useState(false)
+
+  function items() {
+    setItemList(itemList => !itemList)
+  }
+
+  const variable = itemList ? "in-cart" : ""
+
+  const inCart = itemList ? "Remove from cart" : "Add to cart"
+
   return (
-    <li className="">
+    <li className={variable}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick = {items} className="add">{inCart}</button>
     </li>
   );
 }
